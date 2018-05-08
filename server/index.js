17522a415ebb78042fc8ba2a0e465cb9bbee3758
nadaa <nada.saif7@gmail.com>
2018-05-08 20:08:31 +0300

    update the server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
-const express=require('express');
+const express = require('express');
 const bodyParser=require('body-parser')
 
-var Port=process.env.PORT||3000;
+let app = express();
 
-const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));	
+app.get('/users', function (req, res) {
+  
+  res.send({name:'hi'})
+});
 
+let port = 8000;
 
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.listen(Port,()=>console.log(`server started at 3000`));
-
-
-app.get('/',(req,res)=>{
-	//res.send({data:"data"})
-	console.log(console.log("request is recieved"))
-})
-
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
